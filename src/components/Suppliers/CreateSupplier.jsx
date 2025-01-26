f77e9c5b586a1132a8a64fd529f1eeee2743452e
Sowrov9 <sowrovahammed00@gmail.com>
2025-01-26 13:49:30 +0600

    kisoi hoy nai

--- src/components/Suppliers/CreateSupplier.jsx
+++ src/components/Suppliers/CreateSupplier.jsx
@@ -1,59 +1,125 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Cart from './../../cartComponent/Cart';
+
+
 
 const Invoice = () => {
-    const baseUrl="http://localhost/Restaurant_management_home/admin/";
-    const [suppliers,setSuppliers]=useState([]);
-    const[selectSuppliers,setSelectSuppliers]=useState(null);
-    // const [purchaseId,SetPurchaseId]=useState([]);
-    const [items,setItems]=useState([]);
+  const baseUrl = "http://localhost/Restaurant_management_home/admin/";
+  const cart = Cart("purchases")
+  const [suppliers, setSuppliers] = useState([]);
+  const [selectSuppliers, setSelectSuppliers] = useState(null);
+  const [product, setProduct] = useState([])
+  // const [purchaseId,SetPurchaseId]=useState([]);
+  const [summaryCount, setSummaryCount] = useState({
+    discount: 0,
+    tax: 0,
+    subtotal: 0,
+    total: 0
+  })
+  const [item, setItem] = useState({
+    name: "",
+    item_id: 0,
+    qty: 0,
+    price: 0,
+    subtotal: 0
+  })
+  const [items, setItems] = useState([cart.getCart()]);
 
-
-
-    
-
-
-    const fetchSuppliers=()=>{
-        axios.get(baseUrl+"api/supplier/")
-        .then((res)=>{
-            //  console.log(res);
-            setSuppliers(res.data.suppliers);
-        })
+  const handleSetItem = (e) => {
+    const { name, value } = e.target
+    if (name == "product") {
+      let productData = JSON.parse(value)
+      setItem((prev) => ({
+        ...prev,
+        name: productData.name,
+        item_id: productData.id,
+        qty: 0,
+        price: productData.price,
+        subtotal: productData.price
+      }))
     }
 
-    const handleSupplierChange=(e)=>{
-      const{value}=e.target
-      setSelectSuppliers(JSON.parse(value))
+    if (name == "price") {
+      setItem((prev) => ({
+        ...prev,
+        price: value,
+        subtotal: prev.price
+      }))
+    }
+
+    if (name == "qty") {
+      setItem((prev) => ({
+        ...prev,
+        qty: value,
+        subtotal: prev.price * value
+      }))
+
+    }
   }
 
-    // const fetchpurchseId=()=>{
-    //   axios.get(baseUrl+"api/purchase/get_last_id")
-    //   .then((res)=>{
-    //     console.log(res)
-    //     SetPurchaseId(res.data.purchases)
-    //   })
-    // }
+  const handleSetAllItem = (e) => {
+    // console.log(item);
+    cart.save(item)
+    setItems(cart.getCart())
+    setItem({
+      name: "",
+      item_id: 0,
+      qty: 1,
+      price: 0,
+      subtotal: 0,
+      total_discount: 0
+    })
+  }
 
-    const fetchItems=()=>{
-      axios.get(baseUrl+"api/item/")
-      .then((res)=>{
-          // console.log(res.data.items);
-          setItems(res.data.items);
-      })
-      .catch((err)=>{
-        console.log(err);
-        
-      })
-    }
+  // setItem((prev) => ({
+  //   ...prev,
+  //   [name]:value, 
+  // }))
 
-    useEffect(()=>{
-        fetchSuppliers()
-        // fetchpurchseId()
-        fetchItems()
-    },[])
 
-  return (
-   <>
+
+const fetchSuppliers = () => {
+  axios.get(baseUrl + "api/supplier/")
+    .then((res) => {
+      //  console.log(res);
+      setSuppliers(res.data.suppliers);
+    })
+}
+
+const handleSupplierChange = (e) => {
+  const { value } = e.target
+  setSelectSuppliers(JSON.parse(value))
+}
+
+// const fetchpurchseId=()=>{
+//   axios.get(baseUrl+"api/purchase/get_last_id")
+//   .then((res)=>{
+//     console.log(res)
+//     SetPurchaseId(res.data.purchases)
+//   })
+// }
+
+const fetchProduct = () => {
+  axios.get(baseUrl + "api/item/")
+    .then((res) => {
+      // console.log(res.data.items);
+      setProduct(res.data.items);
+    })
+    .catch((err) => {
+      console.log(err);
+
+    })
+}
+
+useEffect(() => {
+  fetchSuppliers()
+  // fetchpurchseId()
+  fetchProduct()
+}, [])
+
+return (
+  <>
     <div className="container my-5">
       <div className="card shadow">
         <div className="card-header bg-primary text-white text-center">
@@ -67,18 +133,18 @@ const Invoice = () => {
               <select onChange={handleSupplierChange} className="form-select mb-2">
                 <option>Select Supplier</option>
                 {
-                    suppliers?.map((data, index)=>{
-                        return(
-                            <option value={JSON.stringify(data)} key={data.id}>{data.name}</option>
-                        )
-                    })
+                  suppliers?.map((data, index) => {
+                    return (
+                      <option value={JSON.stringify(data)} key={data.id}>{data.name}</option>
+                    )
+                  })
 
                 }
               </select>
               <p className="mb-1">Phone: {selectSuppliers && selectSuppliers.mobile}<span id="phone"></span></p>
               <p>Address: {selectSuppliers && selectSuppliers.address}<span id="email"></span></p>
             </div>
-            
+
 
             <div className="col-md-4 text-end" >
               <h5>Invoice Details</h5>
@@ -99,32 +165,33 @@ const Invoice = () => {
                 <th>Item</th>
                 <th>Quantity</th>
                 <th>Unit Price</th>
-                <th>Discount</th>
                 <th>Total</th>
-                <th>Action</th>
+                {/* <th><button onClick={handleDeleteAllItems} className="btn btn-warning">DeleteAll</button></th> */}
               </tr>
               <tr>
                 <td>
-                  <select className="form-select">
+                  <select onChange={handleSetItem} name='product' className="form-select">
                     <option>Select Product</option>
-                    {items?.map((data,i)=>{
-                      return(
+                    {product?.map((data, i) => {
+                      return (
                         <option value={JSON.stringify(data)} key={data.id}>{data.name}</option>
                       )
                     })}
                   </select>
                 </td>
-                <td><input type="number" className="form-control" placeholder="Qty" /></td>
-                <td><input type="number" className="form-control" placeholder="Price" /></td>
-                <td><input type="number" className="form-control" placeholder="Discount" /></td>
+                <td><input onChange={handleSetItem} type="number" name='qty' className="form-control" placeholder="Qty" /></td>
+                <td><input onChange={handleSetItem} type="number" name='price' className="form-control" placeholder="Price" /></td>
+
                 <td>-</td>
                 <td>
-                  <button className="btn btn-info">Add</button>
+                  <button onClick={handleSetAllItem} className="btn btn-info">Add</button>
                 </td>
               </tr>
             </thead>
             <tbody id="data_append">
-              {/* Dynamically added rows will appear here */}
+              {
+
+              }
             </tbody>
             <tfoot>
               <tr>
@@ -158,10 +225,11 @@ const Invoice = () => {
         </div>
       </div>
     </div>
-   
-   
-   </>
-  )
+
+
+  </>
+)
+}
 }
 
 export default Invoice
